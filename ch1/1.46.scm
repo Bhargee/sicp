a5a62d810ae55284d84324c5be35322da4557d08
Bhargava Manja <bhargee.manja@gmail.com>
2015-01-06 18:59:50 -0600

    chapter 1 is done

--- ch1/1.46.scm
+++ ch1/1.46.scm
@@ -1,16 +1,28 @@
 (define (iterative-improve good-enough? improve)
-  (define (fun guess)
-    (if (good-enough? guess)
-        guess
-        (begin (newline) (display guess) (fun (improve guess)))))
-  (lambda (guess) (fun guess)))
+  (define (ret-fun guess)
+      (if (good-enough? guess)
+          guess
+          (ret-fun (improve guess))))
+  (lambda (first-guess) (ret-fun first-guess)))
 
 (define (sqrt x)
-  (define (average a b)
-    (/ (+ a b) 2.0))
-  (define (good-enough? guess)
-    (< (abs (- (* guess guess) x)) .001))
-  (define (improve guess)
-    (average x guess))
-((iterative-improve good-enough? improve) 1.0))
+  ; helpers
+  (define (average a b) (/ (+ a b) 2))
+  (define (square r) (* r r))
+  ; input funcs
+  (define (improve guess) (average guess (/ x guess)))
+  (define (good-enough? guess) (< (abs (- (square guess) x)) 0.001))
+  ; 1.0 is the standard first guest
+  ((iterative-improve good-enough? improve) 1.0))
 
+(sqrt 9.0)
+(sqrt 13876)
+
+(define (fixed-point f first-guess)
+  (define tolerance .00001)
+  (define (improve guess) (f guess))
+  (define (good-enough? guess)
+    (< (abs (- guess (improve guess))) tolerance))
+  ((iterative-improve good-enough? improve) first-guess))
+
+(fixed-point cos 1.0)
