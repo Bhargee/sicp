2486670d0a2505cb7f26cef5381ff0fbe320c7d9
Bhargava Manja <bhargee.manja@gmail.com>
2015-01-17 22:05:11 -0600

    2.29 b working, was simple

--- ch2/2.29.scm
+++ ch2/2.29.scm
@@ -3,6 +3,7 @@
 
 (define (make-branch len structure)
   (list len structure))
+
 ; part a
 (define left-branch car)
 (define (right-branch br)
@@ -12,12 +13,24 @@
 (define branch-structure right-branch)
 
 (define mobile? pair?)
+
 ; part b
 (define (total-weight mobile)
-    (cond
-      ((not (mobile? (branch-structure mobile))) (branch-structure mobile)) 
-      (else (+ 
-              (total-weight (left-branch mobile))
-              (total-weight (right-branch mobile))))))
+      (cond 
+        ((not (mobile? mobile)) mobile)
+        (else (+
+                (total-weight (branch-structure (left-branch mobile))) 
+                (total-weight (branch-structure (right-branch mobile)))))))
+        
 
-
+; testing for part b
+(define level-1-mobile (make-mobile (make-branch 2 1) 
+                                    (make-branch 1 2))) 
+(define level-2-mobile (make-mobile (make-branch 3 level-1-mobile) 
+                                    (make-branch 9 1))) 
+(define level-3-mobile (make-mobile (make-branch 4 level-2-mobile) 
+                                    (make-branch 8 2))) 
+ 
+(total-weight level-1-mobile) ; 3
+(total-weight level-2-mobile) ; 4
+(total-weight level-3-mobile) ; 6
