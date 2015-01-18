ca0447418ff14008eeb8a6a39d45268066dcfa1b
Bhargava Manja <bhargee.manja@gmail.com>
2015-01-17 23:24:27 -0600

    finished 2.32

---
+++ ch2/2.32.scm
@@ -0,0 +1,8 @@
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s)))
+            (first (car s)))
+        (append rest (map (lambda (curr-set)
+                            (cons first curr-set))
+                       rest)))))
